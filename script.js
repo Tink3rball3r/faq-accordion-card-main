3b903cdcf7c48efc4a513cc126d0b4af3aa6de87
Tink3rball3r <matthew_rittenhouse@hotmail.com>
2021-09-07 00:08:36 +0900

    Now menu will close when another is opened.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const accordionButtons = document.querySelectorAll('.accordion');
+const accBtn = document.querySelectorAll('.accordion');
 
-accordionButtons.forEach((accordion) => {
-	accordion.onclick = function () {
-		this.classList.toggle('is-open');
-		this.classList.add('add-pad');
+for (let i = 0; i < accBtn.length; i++) {
+	accBtn[i].addEventListener('click', function () {
+		for (let j = 0; j < accBtn.length; j++) {
+			accBtn[j].classList.remove('is-open');
+			if (j !== i) {
+				accBtn[j].nextElementSibling.style.maxHeight = null;
+			}
+		}
+		this.classList.add('is-open');
 		let content = this.nextElementSibling;
-
 		if (content.style.maxHeight) {
 			content.style.maxHeight = null;
 		} else {
 			content.style.maxHeight = content.scrollHeight + 'px';
 		}
-	};
-});
+	});
+}
